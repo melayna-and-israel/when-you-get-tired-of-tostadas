69b49510cbf3ed99f2982e562d22d4b2d4909d39
israeljimenez <israelj@uci.edu>
2020-12-02 18:22:32 -0800

    continuing setup

--- src/Components/Food/Food.tsx
+++ src/Components/Food/Food.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './food.sass';
 
-import AppClient from '../../Services/DefaultApi';
+import ApiClient from '../../Services/ApiClient'
+import DefaultApi from '../../Services/com.spoonacular/DefaultApi';
 
 
 class Food extends React.Component{
+    client: DefaultApi;
+
+    constructor(props: {} | Readonly<{}>){
+        super(props);
+        this.client = new DefaultApi(new ApiClient());
+        console.log(this.client.searchRecipes("pasta", {cuisine: "italian"}, console.log));
+    }
+
    render(){
        return (
         <p>Food stuff goes here</p>
